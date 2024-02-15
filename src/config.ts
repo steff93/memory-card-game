4e9ad2ad994309635d6deb124043345919fec9a6
stefanB <stefan_boghean@yahoo.com>
2024-02-15 22:24:55 +0200

    removed apoi key, not needed

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
-const API_ITEMS = 12;
-const API_KEY_QUERY = "api_key";
-const API_KEY =
-  "live_lbmkzZkqKz5cYHgxEhBHJFYxR6ethZu4g33gGgP8lCWjbW00cPZwxrDLjecfo9BN";
+const API_ITEMS = 10;
+//const API_KEY_QUERY = "api_key";
+//const API_KEY = "#";
 
-export const API_URL = `https://api.thecatapi.com/v1/images/search?limit=${API_ITEMS}&${API_KEY_QUERY}=${API_KEY}`;
+export const API_URL = `https://api.thecatapi.com/v1/images/search?limit=${API_ITEMS}`;
