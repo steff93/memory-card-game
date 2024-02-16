4cf33e0b253d26bd43e2e6af6c62429373e37ea9
stefanB <stefan_boghean@yahoo.com>
2024-02-16 22:37:01 +0200

    flip the card design

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import MysteryCat from "../../assets/card-front.jpeg";
 import { GameCard } from "../../types";
 import "./Card.scss";
 
@@ -9,9 +11,21 @@ interface CardProps {
 }
 
 const Card = ({ card, selected, disabled, boardDisabled }: CardProps) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleCardFlip = () => {
+    setIsSelected(!isSelected);
+  };
+
   return (
-    <div className="card">
-      <img src={card.url} />
+    <div
+      className={`card ${isSelected ? "flipped" : ""}`}
+      onClick={handleCardFlip}
+    >
+      <div className="card__inner">
+        <img className="card__front" src={MysteryCat} />
+        <img className="card__back" src={card.url} />
+      </div>
     </div>
   );
 };
