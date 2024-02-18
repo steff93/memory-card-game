c7f744d28d86f1a8fd8705386c88387119facaf9
stefanB <stefan_boghean@yahoo.com>
2024-02-18 19:29:06 +0200

    disable mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.scss";
 import Board from "./components/Board/Board";
+import MobileView from "./components/MobileView/MobieView";
 
 function App() {
-  return <Board />;
+  return (
+    <>
+      <Board />
+      <MobileView />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/MobileView/MobieView.tsx
@@ -0,0 +1,5 @@
+const MobileView = () => {
+  return <div className="mobile-view"> Game Not Available On Mobile View</div>;
+};
+
+export default MobileView;
