058d546bd1a0452b8961e815f405bbcd115000d0
stefan_flo <stefan@flothemes.com>
2024-02-18 17:14:16 +0200

    deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import pluginChecker from "vite-plugin-checker";
 
+// deploy!
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginChecker({ typescript: true })],
