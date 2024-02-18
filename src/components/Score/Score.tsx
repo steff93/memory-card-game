aa4cd50632b524c52589685fcf282c6cd6ce356c
stefan_flo <stefan@flothemes.com>
2024-02-18 15:59:29 +0200

    Score styling adjustments

--- src/components/Score/Score.tsx
+++ src/components/Score/Score.tsx
@@ -6,15 +6,17 @@ interface ScoreProps {
 }
 
 const Score = ({ score }: ScoreProps) => {
+  const { player1, player2 } = score;
+
   return (
     <div className="game-board__score">
-      <div> Player 1</div>
-      <div className="score score--total">Pairs: {score.player1.pairs}</div>
-      <div className="score score--moves">Moves: {score.player1.moves}</div>
+      <h3> Player 1</h3>
+      <div className="score score--total">Pairs: {player1.pairs}</div>
+      <div className="score score--moves">Moves: {player1.moves}</div>
 
-      <div> Player 2</div>
-      <div className="score score--total">Pairs: {score.player2.pairs}</div>
-      <div className="score score--moves">Moves: {score.player2.moves}</div>
+      <h3> Player 2</h3>
+      <div className="score score--total">Pairs: {player2.pairs}</div>
+      <div className="score score--moves">Moves: {player2.moves}</div>
     </div>
   );
 };
